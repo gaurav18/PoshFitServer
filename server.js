64a8cadd38ec223a87ba55230776d20b92227c19
Nikhil Sohoni <nikhil@poshmark.com>
2015-11-11 23:29:17 -0800

    Server setup

--- server.js
+++ server.js
@@ -4,4 +4,9 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Hello Http');
 });
+
+function handleRequest(request, response) {
+	response.end('It Works!! Path Hit: ' + request.url);
+}
+
 server.listen(8080);
