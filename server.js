04a306447a20567aeeb69fa677ed0051cbb303a8
Nikhil Sohoni <nikhil@poshmark.com>
2015-11-11 23:38:06 -0800

    Server setup: some simple routes using httpDispatcher

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var http = require('http');
+var dispatcher = require('httpdispatcher');
 
 function handleRequest(request, response) {
-	response.writeHead(200);
-	response.end('It Works!! Path Hit: ' + request.url);
+	try {
+	        //log the request on console
+	        console.log(request.url);
+	        //Disptach
+	        dispatcher.dispatch(request, response);
+	    } catch(err) {
+	        console.log(err);
+	    }
 }
+
+dispatcher.setStatic('resources');
+
+//A sample GET request    
+dispatcher.onGet("/page1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Page One');
+});    
+
+//A sample POST request
+dispatcher.onPost("/post1", function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Got Post Data');
+});
 
 var server = http.createServer(handleRequest);
 
