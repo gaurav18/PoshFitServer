34abe41f6200ff9ba239a69151192ae71ab2d742
Nikhil Sohoni <nikhil@poshmark.com>
2015-11-11 23:33:21 -0800

    Server setup 3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 function handleRequest(request, response) {
-	res.writeHead(200);
+	response.writeHead(200);
 	response.end('It Works!! Path Hit: ' + request.url);
 }
 
