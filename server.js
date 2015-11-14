f45d2427293b90eb7b429b186700527705c19069
badgroover <nikhil@poshmark.com>
2015-11-14 12:33:48 -0800

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var bodyParser = require("body-parser");
 var app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
