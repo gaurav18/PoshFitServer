1b6dcc3e4a47dfd912c12beea81c255f4df83966
badgroover <nikhil@poshmark.com>
2015-11-14 13:35:14 -0800

    Adding session management

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var session = require('express-session');
 var path = require("path");
 var bodyParser = require("body-parser");
 var app = express();
+var session;
 
+app.use(session({secret: '$#$##@#!'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
@@ -22,13 +25,22 @@ var server = app.listen(8080, function () {
 //---------------------------------Routes-----------------------------
 //Login Page
 app.get('/', function (req, res) {
-  console.log('sending login file');
-  res.sendFile('public/login.html' , { root : __dirname});
+  console.log('check session');
+  session = req.session;
+  console.log("session name = "+session.username+", session password is "+session.password);
+  if(session.username) {
+    res.redirect('/leaderboard');
+  }
+  else{
+    res.sendFile('public/login.html' , { root : __dirname});
+  }
 });
 
 app.post('/login',function(req,res){
   var user_name=req.body.user;
   var password=req.body.password;
   console.log("User name = "+user_name+", password is "+password);
+  session.username = user_name;
+  session.password = password;
   res.end("yes");
 });
