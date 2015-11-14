216a18cb2c7bae0251e02f7b706c210ff07c0032
badgroover <nikhil@poshmark.com>
2015-11-14 12:07:39 -0800

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var server = app.listen(8080, function () {
 
 //---------------------------------Routes-----------------------------
 //Login Page
-app.get('/', function (req, res) {
+app.get('/login', function (req, res) {
   console.log('sending login file');
   res.sendfile("login.html");
 });
