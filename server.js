a85615cfc92463c067d99a9e7bbd67b8f6fa168e
badgroover <nikhil@poshmark.com>
2015-11-13 19:53:14 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var path = require("path"),
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.configure(function () {
+    // Define our static file directory, it will be 'public'                                                                                           
+    //app.use(express.static(path.join(__dirname, 'public')));
 });
 
 var server = app.listen(8080, function () {
@@ -10,4 +12,10 @@ var server = app.listen(8080, function () {
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+});
+
+//---------------------------------Routes-----------------------------
+//Login Page
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
